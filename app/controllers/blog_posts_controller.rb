856cbef20b8e18c7f9334526a1a4d2c10ecae37c
chandn12 <chandni@kpstudio.co.uk>
2025-04-09 12:33:08 +0100

    added detroy button that works

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,20 +1,17 @@
 class BlogPostsController < ApplicationController #action controller
+    before_action :set_blog_post, except: [:index, :new, :create]
+
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
     
     def new
         @blog_post = BlogPost.new
-    end
-
-    def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def create
@@ -26,9 +23,31 @@ class BlogPostsController < ApplicationController #action controller
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @blog_post.update(blog_post_params)
+            redirect_to @blog_post
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @blog_post.destroy
+        redirect_to root_path
+    end
+
     private
 
     def blog_post_params 
         params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+            redirect_to root_path
     end
 end
